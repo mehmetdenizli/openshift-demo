28dc48432e4505abae6bc31d4b8b11156ce6d86d
Mehmet Denizli <154231505+mehmetdenizli@users.noreply.github.com>
2026-05-07 22:40:55 +0300

    Update app.py

--- S2I-demo/python-app/app.py
+++ S2I-demo/python-app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    html = "<h3>Hello from Python S2I!</h3>" \
+    html = "<h3>Merhaba from Python S2I!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
            "<b>App Version:</b> {version}<br/>"
     return html.format(hostname=socket.gethostname(), version=os.getenv("APP_VERSION", "1.0.0"))
